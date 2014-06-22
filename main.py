ca98c1324bf58f03871f3888132f7bb8d29ff3d0
Amar Sahinovic <amarsahinovic@gmail.com>
2014-06-22 02:02:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+from flask import Flask, render_template
+from flask.ext.flatpages import FlatPages
+from flask_frozen import Freezer
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
+freezer = Freezer(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', pages=pages)
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/resume/')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/<path:path>/')
+def page(path):
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(port=8000)
---
+++ pages/hello_blog.md
@@ -0,0 +1,7 @@
+title: Hello, Blog!
+date: 2014-07-22
+
+    :::python
+    print("Hello, Blog!")
+
+Well, I finally decided to start a blog. Hopefully, this won't be my first and last post :)
